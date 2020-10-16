e38d43146829c04d98b69d843ee4ee192294613a
Jonathan Barrera <jonathan.barrera@realwear.com>
2020-10-16 16:35:40 -0700

    Commit with working dictation in logs.

--- app/src/main/java/com/realwear/dictationtestapp/MainActivity.kt
+++ app/src/main/java/com/realwear/dictationtestapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.realwear.dictationtestapp
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
+import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val mIntent = createRecognizerIntent()
+    private lateinit var mSpeechRecognizer: SpeechRecognizer
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun startDictation(v: View) {
-        startActivityForResult(mIntent, 1234)
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        mSpeechRecognizer.setRecognitionListener(TestListener())
+        mSpeechRecognizer.startListening(mIntent)
+    }
+
+    fun stopDictation(v:View) {
+        mSpeechRecognizer.stopListening()
     }
 
     fun showCurrentDefaultDictation(v: View) {
@@ -26,19 +36,6 @@ class MainActivity : AppCompatActivity() {
             "voice_recognition_service"
         )
         dictation_textview.text = serviceComponent
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == RESULT_OK && data != null) {
-            onSuccess(data)
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    private fun onSuccess(data: Intent) {
-        val stringArray = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-        val results = stringArray?.joinToString()
-        dictation_textview.text = results
     }
 
     companion object {
@@ -50,5 +47,49 @@ class MainActivity : AppCompatActivity() {
             )
             return intent
         }
+    }
+
+    class TestListener: RecognitionListener {
+        override fun onReadyForSpeech(p0: Bundle?) {
+            Log.d("TestListener", "On ready for speech called.")
+        }
+
+        override fun onBeginningOfSpeech() {
+            Log.d("TestListener", "On beginning of speech called.")
+        }
+
+        override fun onRmsChanged(p0: Float) {
+            Log.d("TestListener", "On rmc called.")
+        }
+
+        override fun onBufferReceived(p0: ByteArray?) {
+            Log.d("TestListener", "On buffer called.")
+        }
+
+        override fun onEndOfSpeech() {
+            Log.d("TestListener", "On end of speech called.")
+        }
+
+        override fun onError(p0: Int) {
+            Log.e("TestListener error", p0.toString() + " called")
+        }
+
+        override fun onResults(p0: Bundle?) {
+            if (p0 == null) {
+                Log.d("TestListener", "On results called with null")
+            }
+            val results = p0?.get(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
+            val text = results[0]
+            Log.d("TestListener", "On results called. " + text)
+        }
+
+        override fun onPartialResults(p0: Bundle?) {
+            Log.d("TestListener", "On partial results called.")
+        }
+
+        override fun onEvent(p0: Int, p1: Bundle?) {
+            Log.d("TestListener", "On event called.")
+        }
+
     }
 }
