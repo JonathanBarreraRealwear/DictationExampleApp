a9831d3a69a9f8ab77ac352252a390f3163fccfa
Jonathan Barrera <jonathan.barrera@realwear.com>
2020-10-19 15:53:11 -0700

    Test with continuous dictation.

--- app/src/main/java/com/realwear/dictationtestapp/MainActivity.kt
+++ app/src/main/java/com/realwear/dictationtestapp/MainActivity.kt
@@ -6,13 +6,13 @@ import android.provider.Settings
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val mIntent = createRecognizerIntent()
     private lateinit var mSpeechRecognizer: SpeechRecognizer
 
     public override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +20,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun startDictation(v: View) {
-        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-        mSpeechRecognizer.setRecognitionListener(TestListener())
-        mSpeechRecognizer.startListening(mIntent)
+    fun asyncDictation(v: View) {
+        async_button.isEnabled = false
+        continuous_dictation.isEnabled = false
+        stop_button.isEnabled = false
+        default_dictation_button.isEnabled = false
+
+        val intent = createRecognizerIntent()
+        intent.putExtra(EXTRA_DICTATION_TYPE, ASYNC_DICTATION)
+        startListening(intent)
     }
 
-    fun stopDictation(v:View) {
-        mSpeechRecognizer.stopListening()
+    fun startDictation(v: View) {
+        async_button.isEnabled = false
+        continuous_dictation.isEnabled = false
+        stop_button.isEnabled = true
+        default_dictation_button.isEnabled = false
+
+        val intent = createRecognizerIntent()
+        intent.putExtra(EXTRA_DICTATION_TYPE, CONTINUOUS_DICTATION)
+        startListening(intent)
+    }
+
+    fun stopDictation(v: View) {
+        async_button.isEnabled = true
+        continuous_dictation.isEnabled = true
+        stop_button.isEnabled = false
+        default_dictation_button.isEnabled = true
+
+        mSpeechRecognizer.destroy()
+    }
+
+    private fun startListening(intent: Intent) {
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        mSpeechRecognizer.setRecognitionListener(TestListener())
+        mSpeechRecognizer.startListening(intent)
     }
 
     fun showCurrentDefaultDictation(v: View) {
@@ -39,6 +66,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val EXTRA_DICTATION_TYPE = "extra_dictation_type"
+        private const val ASYNC_DICTATION = 0
+        private const val CONTINUOUS_DICTATION = 1
         private fun createRecognizerIntent(): Intent {
             val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
             intent.putExtra(
@@ -49,47 +79,68 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    class TestListener: RecognitionListener {
+    inner class TestListener: RecognitionListener {
         override fun onReadyForSpeech(p0: Bundle?) {
-            Log.d("TestListener", "On ready for speech called.")
+            Log.d("TestListener", "onReadyForSpeech called.")
         }
 
         override fun onBeginningOfSpeech() {
-            Log.d("TestListener", "On beginning of speech called.")
+            Log.d("TestListener", "onBeginningOfSpeech called.")
         }
 
         override fun onRmsChanged(p0: Float) {
-            Log.d("TestListener", "On rmc called.")
+            Log.d("TestListener", "onRmsChanged called.")
         }
 
         override fun onBufferReceived(p0: ByteArray?) {
-            Log.d("TestListener", "On buffer called.")
+            Log.d("TestListener", "onBufferReceived called.")
         }
 
         override fun onEndOfSpeech() {
-            Log.d("TestListener", "On end of speech called.")
+            async_button.isEnabled = true
+            continuous_dictation.isEnabled = true
+            stop_button.isEnabled = false
+            default_dictation_button.isEnabled = true
+
+            Log.d("TestListener", "onEndOfSpeech called.")
         }
 
         override fun onError(p0: Int) {
-            Log.e("TestListener error", p0.toString() + " called")
+            Log.e("TestListener", "Error: $p0")
         }
 
         override fun onResults(p0: Bundle?) {
             if (p0 == null) {
-                Log.d("TestListener", "On results called with null")
+                Log.d("TestListener", "onResults called with null.")
+            } else {
+                setResultsText(p0)
             }
-            val results = p0?.get(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
-            val text = results[0]
-            Log.d("TestListener", "On results called. " + text)
         }
 
         override fun onPartialResults(p0: Bundle?) {
-            Log.d("TestListener", "On partial results called.")
+            if (p0 == null) {
+                Log.d("TestListener", "onPartialResults called with null.")
+            } else {
+                setResultsText(p0)
+            }
+        }
+
+        private fun setResultsText(bundle: Bundle) {
+            val results = bundle.get(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
+            var text = results[0]
+
+            text = if (results.size > 1) {
+                TextUtils.join(" ", results)
+            } else {
+                results[0]
+            }
+
+            dictation_textview.text = text
+
         }
 
         override fun onEvent(p0: Int, p1: Bundle?) {
-            Log.d("TestListener", "On event called.")
+            Log.d("TestListener", "onEvent called.")
         }
-
     }
 }
